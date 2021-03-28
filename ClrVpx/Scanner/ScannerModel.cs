38139ba564c2925b750ee683cc0fabe0bfae0274
Tibor <Stoj>
2021-03-28 23:38:26 +0800

    added some click handling

--- ClrVpx/Scanner/ScannerModel.cs
+++ ClrVpx/Scanner/ScannerModel.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
+using PropertyChanged;
 
 namespace ClrVpx.Scanner
 {
+    [AddINotifyPropertyChangedInterface]
     public class ScannerModel
     {
         public ScannerModel()
+        {
+            Scan = new ActionCommand(StartScan);
+        }
+
+        private void StartScan()
         {
             Results = new ObservableCollection<User>();
             Results.Add(new User { Id = 1, Name = "John Doe", Birthday = new DateTime(1971, 7, 23) });
@@ -14,6 +22,8 @@ namespace ClrVpx.Scanner
         }
 
         public ObservableCollection<User> Results { get; set; }
+
+        public ICommand Scan { get; set; }
     }
 
     public class User
---
+++ Utils/ActionCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ClrVpx.Scanner
+{
+    public class ActionCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public ActionCommand(Action execute, Func<bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute ?? (() => true);
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute();
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
