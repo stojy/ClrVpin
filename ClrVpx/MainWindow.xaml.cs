74612eb70abcdb4f38479838749ec300232947bd
Tibor <Stoj>
2021-03-28 23:02:43 +0800

    added a dummy datagrid

--- ClrVpx/MainWindow.xaml.cs
+++ ClrVpx/MainWindow.xaml.cs
@@ -1,28 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using PropertyChanged;
 
 namespace ClrVpx
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
+    [AddINotifyPropertyChangedInterface]
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
+            DataContext = this;
+         
             InitializeComponent();
+
+            Scanner = new ScannerModel();
+            Scanner.Results = new ObservableCollection<User>();
+            Scanner.Results.Add(new User { Id = 1, Name = "John Doe", Birthday = new DateTime(1971, 7, 23) });
+            Scanner.Results.Add(new User { Id = 2, Name = "Jane Doe", Birthday = new DateTime(1974, 1, 17) });
+            Scanner.Results.Add(new User { Id = 3, Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2) });
+
         }
+
+        public ScannerModel Scanner { get; set; }
+    }
+
+    public class ScannerModel
+    {
+        public ObservableCollection<User> Results { get; set; }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Birthday { get; set; }
     }
 }
