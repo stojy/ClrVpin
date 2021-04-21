346079498206f640462fc806d15772a67183d9fc
Stojy <tibor@stoj.net>
2021-04-21 22:20:48 +0800

    split scanner utils into a separate class

--- ClrVpin/Scanner/Scanner.cs
+++ ClrVpin/Scanner/Scanner.cs
@@ -2,14 +2,11 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Data;
 using System.Windows.Input;
-using System.Xml.Linq;
 using ClrVpin.Models;
-using NLog;
 using PropertyChanged;
 using Utils;
 
@@ -54,12 +51,12 @@ namespace ClrVpin.Scanner
             _scannerWindow.Closed += (_, _) => _parentWindow.Show();
         }
 
-        private void ShowResults()
+        private void ShowResults(List<Tuple<string, long>> unknownFiles)
         {
             var scannerResults = new ScannerResults(_scannerWindow, Games);
             scannerResults.Show();
 
-            var scannerStatistics = new ScannerStatistics(Games, _scanStopWatch, _unknownFiles);
+            var scannerStatistics = new ScannerStatistics(Games, _scanStopWatch, unknownFiles);
             scannerStatistics.Show(_scannerWindow, scannerResults.Window);
 
             var explorerWindow = new ScannerExplorer(Games);
@@ -125,146 +122,24 @@ namespace ClrVpin.Scanner
 
             _scanStopWatch = Stopwatch.StartNew();
 
-            var games = GetDatabase();
+            var games = ScannerUtils.GetDatabase();
 
             // todo; retrieve 'missing games' from spreadsheet
-            
-            Check(games);
-            
-            Fix(games);
+
+            var unknownFiles = ScannerUtils.Check(games);
+
+            ScannerUtils.Fix(games);
 
             Games = new ObservableCollection<Game>(games);
 
             _scanStopWatch.Stop();
 
-            ShowResults();
-        }
-
-        private void Check(List<Game> games)
-        {
-            // for the configured content types only.. check the installed content files against those specified in the database
-            var checkContentTypes = Content.SupportedTypes.Where(type => Config.CheckContentTypes.Contains(type.Type));
-            checkContentTypes.ForEach(contentSetup =>
-            {
-                var mediaFiles = GetMedia(contentSetup);
-                var unknownMedia = AddMedia(games, mediaFiles, contentSetup.GetContentHits);
-
-                // todo; add non-media content, e.g. tables and b2s
-
-                _unknownFiles.AddRange(unknownMedia);
-            });
-
-            CheckMissing(games);
-        }
-
-        private static void Fix(List<Game> games)
-        {
-            games.ForEach(game =>
-            {
-                game.Content.ContentHitsCollection.ForEach(contentHitCollection =>
-                {
-                    if (contentHitCollection.Hits.Any(hit => hit.Type == HitType.Valid))
-                    {
-                        // all hit files can be deleted :)
-                        contentHitCollection.Hits.Where(hit => hit.Type != HitType.Valid).ForEach(hit =>
-                        {
-                            switch (hit.Type)
-                            {
-                                case HitType.DuplicateExtension:
-                                case HitType.Fuzzy:
-                                case HitType.TableName:
-                                case HitType.WrongCase:
-                                    Delete(hit);
-                                    break;
-                            }
-                        });
-                    }
-                });
-            });
-        }
-
-        private static void Delete(Hit hit)
-        {
-            if (Config.FixHitTypes.Contains(hit.Type))
-                Logger.Info($"deleting: type={hit.Type}, content={hit.ContentType}, path={hit.Path}");
-        }
-
-        private static void CheckMissing(List<Game> games)
-        {
-            games.ForEach(game =>
-            {
-                // add missing content
-                game.Content.ContentHitsCollection.ForEach(contentHitCollection =>
-                {
-                    if (!contentHitCollection.Hits.Any(hit => hit.Type == HitType.Valid || hit.Type == HitType.WrongCase))
-                        contentHitCollection.Add(HitType.Missing, game.Description);
-                });
-            });
-        }
-
-        private static IEnumerable<Tuple<string, long>> AddMedia(IReadOnlyCollection<Game> games, IEnumerable<string> mediaFiles, Func<Game, ContentHits> getContentHits)
-        {
-            var unknownMediaFiles = new List<Tuple<string, long>>();
-
-            mediaFiles.ForEach(mediaFile =>
-            {
-                Game matchedGame;
-
-                // check for hit..
-                // - skip hit types that aren't configured
-                // - only 1 hit per file.. but a game can have multiple hits.. with a maximum of 1 valid hit
-                // - todo; fuzzy match.. e.g. partial matches, etc.
-                if ((matchedGame = games.FirstOrDefault(game => game.Description == Path.GetFileNameWithoutExtension(mediaFile))) != null)
-                {
-                    // if a match already exists, then assume this match is a duplicate name with wrong extension
-                    // - file extension order is important as it determines the priority of the preferred extension
-                    var contentHits = getContentHits(matchedGame);
-                    contentHits.Add(contentHits.Hits.Any(hit => hit.Type == HitType.Valid) ? HitType.DuplicateExtension : HitType.Valid, mediaFile);
-                }
-
-                if ((matchedGame = games.FirstOrDefault(game =>
-                    string.Equals(game.Description, Path.GetFileNameWithoutExtension(mediaFile), StringComparison.CurrentCultureIgnoreCase))) != null)
-                    getContentHits(matchedGame).Add(HitType.WrongCase, mediaFile);
-                else if ((matchedGame = games.FirstOrDefault(game => game.TableFile == Path.GetFileNameWithoutExtension(mediaFile))) != null)
-                    getContentHits(matchedGame).Add(HitType.TableName, mediaFile);
-                else
-                    unknownMediaFiles.Add(new Tuple<string, long>(mediaFile, new FileInfo(mediaFile).Length));
-            });
-
-            return unknownMediaFiles;
-        }
-
-        private static List<Game> GetDatabase()
-        {
-            var file = $@"{Config.VpxFrontendFolder}\Databases\Visual Pinball\Visual Pinball.xml";
-            var doc = XDocument.Load(file);
-            if (doc.Root == null)
-                throw new Exception("Failed to load database");
-
-            var menu = doc.Root.Deserialize<Menu>();
-            var number = 1;
-            menu.Games.ForEach(g =>
-            {
-                g.Number = number++;
-                g.Ipdb = g.IpdbId ?? g.IpdbNr;
-            });
-
-            return menu.Games;
-        }
-
-        private static IEnumerable<string> GetMedia(ContentType contentType)
-        {
-            var files = contentType.Extensions.Select(ext => Directory.GetFiles(contentType.QualifiedFolder, ext));
-
-            return files.SelectMany(x => x).ToList();
+            ShowResults(unknownFiles);
         }
 
         private readonly IEnumerable<FeatureType> _fixHitTypes;
         private readonly MainWindow _parentWindow;
         private Window _scannerWindow;
         private Stopwatch _scanStopWatch;
-        private readonly List<Tuple<string, long>> _unknownFiles = new List<Tuple<string, long>>();
-        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
-
     }
 }
---
+++ ClrVpin/Scanner/ScannerUtils.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using ClrVpin.Models;
+using NLog;
+using Utils;
+
+namespace ClrVpin.Scanner
+{
+    public static class ScannerUtils
+    {
+        public static List<Tuple<string, long>> Check(List<Game> games)
+        {
+            var unknownFiles = new List<Tuple<string, long>>();
+
+            // for the configured content types only.. check the installed content files against those specified in the database
+            var checkContentTypes = Content.SupportedTypes.Where(type => Config.CheckContentTypes.Contains(type.Type));
+            checkContentTypes.ForEach(contentSetup =>
+            {
+                var mediaFiles = GetMedia(contentSetup);
+                var unknownMedia = AddMedia(games, mediaFiles, contentSetup.GetContentHits);
+
+                // todo; add non-media content, e.g. tables and b2s
+
+                unknownFiles.AddRange(unknownMedia);
+            });
+
+            CheckMissing(games);
+
+            return unknownFiles;
+        }
+
+        public static List<Game> GetDatabase()
+        {
+            var file = $@"{Config.VpxFrontendFolder}\Databases\Visual Pinball\Visual Pinball.xml";
+            var doc = XDocument.Load(file);
+            if (doc.Root == null)
+                throw new Exception("Failed to load database");
+
+            var menu = doc.Root.Deserialize<Menu>();
+            var number = 1;
+            menu.Games.ForEach(g =>
+            {
+                g.Number = number++;
+                g.Ipdb = g.IpdbId ?? g.IpdbNr;
+            });
+
+            return menu.Games;
+        }
+
+        public static void Fix(List<Game> games)
+        {
+            games.ForEach(game =>
+            {
+                game.Content.ContentHitsCollection.ForEach(contentHitCollection =>
+                {
+                    if (contentHitCollection.Hits.Any(hit => hit.Type == HitType.Valid))
+                        // all hit files can be deleted :)
+                        contentHitCollection.Hits.Where(hit => hit.Type != HitType.Valid).ForEach(hit =>
+                        {
+                            switch (hit.Type)
+                            {
+                                case HitType.DuplicateExtension:
+                                case HitType.Fuzzy:
+                                case HitType.TableName:
+                                case HitType.WrongCase:
+                                    Delete(hit);
+                                    break;
+                            }
+                        });
+                });
+            });
+        }
+
+
+        private static void CheckMissing(List<Game> games)
+        {
+            games.ForEach(game =>
+            {
+                // add missing content
+                game.Content.ContentHitsCollection.ForEach(contentHitCollection =>
+                {
+                    if (!contentHitCollection.Hits.Any(hit => hit.Type == HitType.Valid || hit.Type == HitType.WrongCase))
+                        contentHitCollection.Add(HitType.Missing, game.Description);
+                });
+            });
+        }
+
+        private static IEnumerable<Tuple<string, long>> AddMedia(IReadOnlyCollection<Game> games, IEnumerable<string> mediaFiles, Func<Game, ContentHits> getContentHits)
+        {
+            var unknownMediaFiles = new List<Tuple<string, long>>();
+
+            mediaFiles.ForEach(mediaFile =>
+            {
+                Game matchedGame;
+
+                // check for hit..
+                // - skip hit types that aren't configured
+                // - only 1 hit per file.. but a game can have multiple hits.. with a maximum of 1 valid hit
+                // - todo; fuzzy match.. e.g. partial matches, etc.
+                if ((matchedGame = games.FirstOrDefault(game => game.Description == Path.GetFileNameWithoutExtension(mediaFile))) != null)
+                {
+                    // if a match already exists, then assume this match is a duplicate name with wrong extension
+                    // - file extension order is important as it determines the priority of the preferred extension
+                    var contentHits = getContentHits(matchedGame);
+                    contentHits.Add(contentHits.Hits.Any(hit => hit.Type == HitType.Valid) ? HitType.DuplicateExtension : HitType.Valid, mediaFile);
+                }
+
+                if ((matchedGame = games.FirstOrDefault(game =>
+                    string.Equals(game.Description, Path.GetFileNameWithoutExtension(mediaFile), StringComparison.CurrentCultureIgnoreCase))) != null)
+                    getContentHits(matchedGame).Add(HitType.WrongCase, mediaFile);
+                else if ((matchedGame = games.FirstOrDefault(game => game.TableFile == Path.GetFileNameWithoutExtension(mediaFile))) != null)
+                    getContentHits(matchedGame).Add(HitType.TableName, mediaFile);
+                else
+                    unknownMediaFiles.Add(new Tuple<string, long>(mediaFile, new FileInfo(mediaFile).Length));
+            });
+
+            return unknownMediaFiles;
+        }
+
+        private static IEnumerable<string> GetMedia(ContentType contentType)
+        {
+            var files = contentType.Extensions.Select(ext => Directory.GetFiles(contentType.QualifiedFolder, ext));
+
+            return files.SelectMany(x => x).ToList();
+        }
+
+        private static void Delete(Hit hit)
+        {
+            if (Config.FixHitTypes.Contains(hit.Type))
+                Logger.Info($"deleting: type={hit.Type}, content={hit.ContentType}, path={hit.Path}");
+        }
+
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+    }
+}
