10f3e4897caf056db6b6a2bb90dfd03d284a0279
Stojy <tibor@stoj.net>
2021-05-14 22:47:13 +0800

    added a colored icon for the valid/smelly status

--- ClrVpin/Converters/BoolToStringConverter.cs
+++ ClrVpin/Converters/BoolToStringConverter.cs
@@ -1,21 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Globalization;
-using System.Linq;
 using System.Windows;
 using System.Windows.Data;
 
-namespace ClrVpin.Settings
+namespace ClrVpin.Converters
 {
-    [ValueConversion(typeof(List<string>), typeof(string))]
-    public class ListToStringConverter : IValueConverter
+    [ValueConversion(typeof(bool), typeof(string))]
+    public class BoolToStringConverter : IValueConverter
     {
+        public string True { get; set; }
+        public string False { get; set; }
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (targetType != typeof(string) || !(value is IEnumerable<string>))
+            if (!(value is bool))
                 return DependencyProperty.UnsetValue;
 
-            return string.Join(", ", ((IEnumerable<string>) value).ToArray());
+            return (bool) value ? True : False;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
---
+++ ClrVpin/Converters/ListToStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ClrVpin.Converters
+{
+    [ValueConversion(typeof(List<string>), typeof(string))]
+    public class ListToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(string) || !(value is IEnumerable<string>))
+                return DependencyProperty.UnsetValue;
+
+            return string.Join(", ", ((IEnumerable<string>) value).ToArray());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
