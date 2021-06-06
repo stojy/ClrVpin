e5637802ffcc35215dcc4aa1fe8e3712cfdccb61
stoj <tibor@stoj.net>
2021-06-06 15:21:28 +0800

    #10 - add file count and size stats for unknown files grouped by content type

--- ClrVpin/Scanner/FixFileDetail.cs
+++ ClrVpin/Scanner/FixFileDetail.cs
@@ -4,13 +4,15 @@ namespace ClrVpin.Scanner
 {
     public class FixFileDetail : FileDetail
     {
-        public FixFileDetail(HitTypeEnum hitType, bool deleted, bool renamed, string path, long size) : base(path, size)
+        public FixFileDetail(ContentTypeEnum contentTypeEnum, HitTypeEnum hitType, bool deleted, bool renamed, string path, long size) : base(path, size)
         {
+            ContentType = contentTypeEnum;
             HitType = hitType;
             Deleted = deleted;
             Renamed = renamed;
         }
 
+        public ContentTypeEnum ContentType { get; }
         public HitTypeEnum HitType { get; }
         public bool Deleted { get; set; }
         public bool Renamed { get; }
