0d53eb220aab42a3b5f66067c0be810a149f63b4
stoj <tibor@stoj.net>
2022-04-09 22:30:06 +0800

    #28 - skip files whose destination file name has invalid characters

---
+++ ClrVpin.Tests/Shared/FileUtilsTests.cs
@@ -0,0 +1,26 @@
+﻿using ClrVpin.Shared;
+using NUnit.Framework;
+
+namespace ClrVpin.Tests.Shared
+{
+    [TestFixture]
+    internal class FileUtilsTests
+    {
+        [Test]
+        [TestCase(null, true)]
+        [TestCase("file", false)]
+        [TestCase("file.txt", false)]
+        [TestCase("file/", false)]
+        [TestCase("path/file", false)]
+        [TestCase("file:", true)]
+        [TestCase("fi:le", true)]
+        [TestCase(@"directory\file", false)]
+        [TestCase(@"directory\file", false)]
+        [TestCase("Star Trek: The Next Generation (Williams 1993).mp3", true)]
+        [TestCase(@"C:\vp\apps\PinballX\Media\Visual Pinball\Launch Audio\Star Trek: The Next Generation (Williams 1993).mp3", true)]
+        public void TestContainsInvalidChars(string file, bool expectedInvalid)
+        {
+            Assert.That(file.HasInvalidFileNameChars(), Is.EqualTo(expectedInvalid));
+        }
+    }
+}
