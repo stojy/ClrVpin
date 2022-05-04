c409a752d94ffc0281870d89b8b4a13ba66604dc
stoj <tibor@stoj.net>
2022-05-04 22:19:23 +0800

    #25: added importer matching and ignore criteria

--- ClrVpin/Importer/ImporterViewModel.cs
+++ ClrVpin/Importer/ImporterViewModel.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Text.Json;
 using System.Windows;
 using System.Windows.Input;
 using ClrVpin.Controls;
-using ClrVpin.Importer.Vps;
 using ClrVpin.Logging;
+using ClrVpin.Models;
+using ClrVpin.Models.Rebuilder;
+using ClrVpin.Models.Settings;
 using ClrVpin.Shared;
 using PropertyChanged;
 using Utils;
+using Utils.Extensions;
+using Game = ClrVpin.Importer.Vps.Game;
 
 namespace ClrVpin.Importer
 {
@@ -21,47 +27,28 @@ namespace ClrVpin.Importer
 
             //DestinationContentTypeSelectedCommand = new ActionCommand(UpdateIsValid);
 
-            //MatchCriteriaTypesView = new ListCollectionView(CreateMatchCriteriaTypes().ToList());
+            CreateMatchCriteriaTypes();
             
-            //IgnoreOptionsTypesView = new ListCollectionView(CreateIgnoreOptions().ToList());
+            CreateIgnoreOptions();
 
-            //MergeOptionsTypesView = new ListCollectionView(CreateMergeOptions().ToList());
-
-            //SourceFolderModel = new FolderTypeModel("Source", Settings.Rebuilder.SourceFolder, folder =>
-            //{
-            //    Settings.Rebuilder.SourceFolder = folder;
-            //    TryUpdateDestinationFolder(folder);
-            //});
-
-            //_destinationContentTypes = Model.Settings.GetFixableContentTypes().Select(x => x.Description);
-            //DestinationContentTypes = new ObservableCollection<string>(_destinationContentTypes);
-
-            //IgnoreWordsString = string.Join(", ", Settings.Rebuilder.IgnoreIWords);
-            //IgnoreWordsChangedCommand = new ActionCommand(IgnoreWordsChanged);
+            IgnoreWordsString = string.Join(", ", Settings.Importer.IgnoreIWords);
+            IgnoreWordsChangedCommand = new ActionCommand(IgnoreWordsChanged);
 
             UpdateIsValid();
         }
 
         public bool IsValid { get; set; }
 
-        //public ListCollectionView MatchCriteriaTypesView { get; set; }
-        //public ListCollectionView IgnoreOptionsTypesView { get; set; }
-        //public ListCollectionView MergeOptionsTypesView { get; set; }
-
-        //public FolderTypeModel SourceFolderModel { get; set; }
-        //public ObservableCollection<string> DestinationContentTypes { get; set; }
-
-        //public ICommand DestinationContentTypeSelectedCommand { get; set; }
         //public ObservableCollection<Game> Games { get; set; }
         public ICommand StartCommand { get; set; }
         public Models.Settings.Settings Settings { get; } = Model.Settings;
 
-        //public string IgnoreWordsString { get; set; }
-        //public ICommand IgnoreWordsChangedCommand { get; set; }
+        public string IgnoreWordsString { get; set; }
+        public ICommand IgnoreWordsChangedCommand { get; set; }
 
         //public FeatureType IgnoreIfNotNewerFeature { get; set; }
         //public FeatureType IgnoreIfSmallerFeature { get; set; }
-        //public FeatureType IgnoreIfContainsWordsFeature { get; set; }
+        public FeatureType IgnoreIfContainsWordsFeature { get; set; }
         //public FeatureType DeleteIgnoredFilesOptionFeature { get; set; }
         //public FeatureType IgnoreSelectClearAllFeature { get; set; }
 
@@ -87,113 +74,62 @@ namespace ClrVpin.Importer
                 Model.SettingsManager.Write();
                 parent.Show();
             };
-
-            Start();
         }
 
-        //private void IgnoreWordsChanged()
-        //{
-        //    Settings.Rebuilder.IgnoreIWords = IgnoreWordsString == null ? new List<string>() : IgnoreWordsString.Split(",").Select(x => x.Trim().ToLower()).ToList();
-        //}
+        private void IgnoreWordsChanged()
+        {
+            Settings.Importer.IgnoreIWords = IgnoreWordsString == null ? new List<string>() : IgnoreWordsString.Split(",").Select(x => x.Trim().ToLower()).ToList();
+        }
 
-        private void UpdateIsValid() => IsValid = !string.IsNullOrEmpty(Settings.Rebuilder.DestinationContentType);
+        private void UpdateIsValid() => IsValid = true;
 
-        //private void TryUpdateDestinationFolder(string folder)
-        //{
-        //    // attempt to assign destination folder automatically based on the specified folder
-        //    var contentType = _destinationContentTypes.FirstOrDefault(c => folder.ToLower().EndsWith(c.ToLower()));
-        //    Settings.Rebuilder.DestinationContentType = contentType;
+        private void CreateMatchCriteriaTypes()
+        {
+            // show all match criteria types
+            // - only fuzzy is supported, but using a list for consistency with scanner and rebuilder
+            var featureTypes = StaticSettings.MatchTypes.Where(x => x.Enum.In(HitTypeEnum.Fuzzy)).Select(matchType =>
+            {
+                var featureType = new FeatureType((int)matchType.Enum)
+                {
+                    Description = matchType.Description,
+                    Tip = matchType.Tip,
+                    IsSupported = true,
+                    IsActive = Settings.Importer.SelectedMatchTypes.Contains(matchType.Enum),
+                    SelectedCommand = new ActionCommand(() => Settings.Importer.SelectedMatchTypes.Toggle(matchType.Enum)),
+                    IsHighlighted = matchType.IsHighlighted,
+                    IsHelpSupported = matchType.HelpUrl != null,
+                    HelpAction = new ActionCommand(() => Process.Start(new ProcessStartInfo(matchType.HelpUrl) { UseShellExecute = true }))
+                };
 
-        //    UpdateIsValid();
-        //}
+                return featureType;
+            }).ToList();
 
-        //private IEnumerable<FeatureType> CreateMatchCriteriaTypes()
-        //{
-        //    // show all match criteria types
-        //    // - except for unknown and unsupported which are used 'under the hood' for subsequent reporting
-        //    var featureTypes = StaticSettings.MatchTypes.Where(x => !x.Enum.In(HitTypeEnum.CorrectName, HitTypeEnum.Unknown, HitTypeEnum.Unsupported)).Select(matchType =>
-        //    {
-        //        var featureType = new FeatureType((int) matchType.Enum)
-        //        {
-        //            Description = matchType.Description,
-        //            Tip = matchType.Tip,
-        //            IsSupported = true,
-        //            IsActive = Settings.Rebuilder.SelectedMatchTypes.Contains(matchType.Enum),
-        //            SelectedCommand = new ActionCommand(() => Settings.Rebuilder.SelectedMatchTypes.Toggle(matchType.Enum)),
-        //            IsHighlighted = matchType.IsHighlighted,
-        //            IsHelpSupported = matchType.HelpUrl != null,
-        //            HelpAction = new ActionCommand(() => Process.Start(new ProcessStartInfo(matchType.HelpUrl) {UseShellExecute = true}))
-        //        };
+            // create separate property for each so they can be referenced individually in the UI
+            MatchFuzzy = featureTypes.First(x => x.Id == (int)HitTypeEnum.Fuzzy);
+        }
 
-        //        return featureType;
-        //    }).ToList();
+        public FeatureType MatchFuzzy { get; private set; }
 
-        //    return featureTypes.Concat(new[] { FeatureType.CreateSelectAll(featureTypes) });
-        //}
+        private void CreateIgnoreOptions()
+        {
+            // show all ignore options
+            // - only fuzzy is supported, but using a list for consistency with scanner and rebuilder
+            var featureTypes = StaticSettings.IgnoreOptions.Select(ignoreOption =>
+            {
+                var featureType = new FeatureType((int)ignoreOption.Enum)
+                {
+                    Description = ignoreOption.Description,
+                    Tip = ignoreOption.Tip,
+                    IsSupported = true,
+                    IsActive = Settings.Importer.SelectedIgnoreOptions.Contains(ignoreOption.Enum),
+                    SelectedCommand = new ActionCommand(() => Settings.Importer.SelectedIgnoreOptions.Toggle(ignoreOption.Enum))
+                };
 
-        //private IEnumerable<FeatureType> CreateIgnoreOptions()
-        //{
-        //    // show all merge options
-        //    var featureTypes = StaticSettings.IgnoreOptions.Select(ignoreOption =>
-        //    {
-        //        var featureType = new FeatureType((int) ignoreOption.Enum)
-        //        {
-        //            Description = ignoreOption.Description,
-        //            Tip = ignoreOption.Tip,
-        //            IsSupported = true,
-        //            IsActive = Settings.Rebuilder.SelectedIgnoreOptions.Contains(ignoreOption.Enum),
-        //            SelectedCommand = new ActionCommand(() => Settings.Rebuilder.SelectedIgnoreOptions.Toggle(ignoreOption.Enum))
-        //        };
+                return featureType;
+            }).ToList();
 
-        //        return featureType;
-        //    }).ToList();
-
-        //    IgnoreIfContainsWordsFeature = featureTypes.First(x => x.Id == (int) IgnoreOptionEnum.IgnoreIfContainsWords);
-        //    IgnoreIfSmallerFeature = featureTypes.First(x => x.Id == (int) IgnoreOptionEnum.IgnoreIfSmaller);
-        //    IgnoreIfNotNewerFeature = featureTypes.First(x => x.Id == (int) IgnoreOptionEnum.IgnoreIfNotNewer);
-            
-        //    // delete ignored isn't technically an ignored option.. but added here to keep it consistent visually
-        //    DeleteIgnoredFilesOptionFeature = CreateDeleteIgnoredFilesOption();
-        //    featureTypes.Add(DeleteIgnoredFilesOptionFeature);
-
-        //    IgnoreSelectClearAllFeature = FeatureType.CreateSelectAll(featureTypes);
-
-        //    return featureTypes;
-        //}
-
-        //public FeatureType CreateDeleteIgnoredFilesOption()
-        //{
-        //    var feature = new FeatureType(-1)
-        //    {
-        //        Description = StaticSettings.DeleteIgnoredFilesOption.Description,
-        //        Tip = StaticSettings.DeleteIgnoredFilesOption.Tip,
-        //        IsSupported = true,
-        //        IsActive = Settings.Rebuilder.DeleteIgnoredFiles,
-        //        SelectedCommand = new ActionCommand(() => Settings.Rebuilder.DeleteIgnoredFiles = !Settings.Rebuilder.DeleteIgnoredFiles)
-        //    };
-
-        //    return feature;
-        //}
-
-        //private IEnumerable<FeatureType> CreateMergeOptions()
-        //{
-        //    // show all merge options
-        //    var featureTypes = StaticSettings.MergeOptions.Select(mergeOption =>
-        //    {
-        //        var featureType = new FeatureType((int) mergeOption.Enum)
-        //        {
-        //            Description = mergeOption.Description,
-        //            Tip = mergeOption.Tip,
-        //            IsSupported = true,
-        //            IsActive = Settings.Rebuilder.SelectedMergeOptions.Contains(mergeOption.Enum),
-        //            SelectedCommand = new ActionCommand(() => Settings.Rebuilder.SelectedMergeOptions.Toggle(mergeOption.Enum))
-        //        };
-
-        //        return featureType;
-        //    }).ToList();
-
-        //    return featureTypes.Concat(new[] { FeatureType.CreateSelectAll(featureTypes) });
-        //}
+            IgnoreIfContainsWordsFeature = featureTypes.First(x => x.Id == (int)IgnoreOptionEnum.IgnoreIfContainsWords);
+        }
 
         private async void Start()
         {
