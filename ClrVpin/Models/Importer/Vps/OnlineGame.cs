935a1ea93d9cc77d8557846e3fe2b02f228054fb
stoj <tibor@stoj.net>
2022-05-15 16:58:56 +0800

    importer: separated OnlineGame VM properteis into a derived class

---
+++ ClrVpin/Models/Importer/Vps/OnlineGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using ClrVpin.Shared;
+
+namespace ClrVpin.Models.Importer.Vps;
+
+// ReSharper disable ClassNeverInstantiated.Global - required for collections as r# doesn't realize this is a json deserialized object
+public class OnlineGame : OnlineGameBase
+{
+    // view model properties
+    public Dictionary<string, FileCollection> AllFiles { get; set; }
+    public IEnumerable<File> AllFilesList { get; set; }
+    public List<ImageFile> ImageFiles { get; set; }
+
+    public int Index { get; set; }
+    public UrlSelection ImageUrlSelection { get; set; }
+    public string YearString { get; set; }
+    public bool IsOriginal => TableUtils.IsOriginal(Manufacturer);
+
+    // reference to the highest fuzzy ranked DB match
+    public GameHit Hit { get; set; }
+    public bool IsMatched => Hit != null;
+}
