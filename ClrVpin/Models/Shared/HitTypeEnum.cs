aae752f9c3e0d2b6ecb2783605aedd75bc0f1b19
stoj <tibor@stoj.net>
2022-07-31 21:09:52 +0800

    general: renamed fix type.. "unknown file" instead of the incorrect "unknown table file"

--- ClrVpin/Models/Shared/HitTypeEnum.cs
+++ ClrVpin/Models/Shared/HitTypeEnum.cs
@@ -17,7 +17,7 @@ namespace ClrVpin.Models.Shared
 
         [Description("Missing File")] Missing,
         
-        [Description("Unknown Table File")] Unknown,    // supported file extension, but unable to be matched against a game/table
+        [Description("Unknown File")] Unknown,    // supported file extension, but unable to be matched against a game/table
         
         [Description("Unsupported File Type")] Unsupported    // unsupported file type (extension).. and thus also can't be matched against a game/table
     }
