dcd21ea724784e15f4bb3a54c26a53837244de2a
stoj <tibor@stoj.net>
2022-08-20 16:04:28 +0800

    fixed null ref error with the update game item dialog box.. symptom is the dialog box has a transparent background

--- ClrVpin/Extensions/BindingExtensions.cs
+++ ClrVpin/Extensions/BindingExtensions.cs
@@ -7,6 +7,6 @@ public static class BindingExtensions
     public static object GetValue(this BindingExpression bindingExpression)
     {
         // use reflection to retrieve the bound value of the binding expression
-        return bindingExpression?.ResolvedSource.GetType().GetProperty(bindingExpression.ResolvedSourcePropertyName)?.GetValue(bindingExpression.ResolvedSource);
+        return bindingExpression?.ResolvedSource?.GetType().GetProperty(bindingExpression.ResolvedSourcePropertyName)?.GetValue(bindingExpression.ResolvedSource);
     }
 }
