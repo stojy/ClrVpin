8758179ab09eaa791636cb02e6c2612501c0a856
stoj <tibor@stoj.net>
2022-08-25 23:10:50 +0800

    fuzzy: separate update option (with icon) for updating an unmatched vs matched local DB entry

--- ClrVpin/Converters/BoolToStyleConverter.cs
+++ ClrVpin/Converters/BoolToStyleConverter.cs
@@ -3,15 +3,14 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 
-namespace ClrVpin.Converters
+namespace ClrVpin.Converters;
+
+public class BoolToStyleConverter : IValueConverter
 {
-    public class BoolToStyleConverter : IValueConverter
-    {
-        public Style True { get; set; }
-        public Style False { get; set; }
+    public Style True { get; set; }
+    public Style False { get; set; }
 
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is true ? True : False;
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is true ? True : False;
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
-    }
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
 }
