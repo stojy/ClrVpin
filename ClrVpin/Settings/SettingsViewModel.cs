844aacfecd2c8d1945f745dc81990a64f5f986a8
stoj <tibor@stoj.net>
2022-09-06 23:47:37 +0800

    performance: fixed binding error with BindingProxy

--- ClrVpin/Settings/SettingsViewModel.cs
+++ ClrVpin/Settings/SettingsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using ClrVpin.Controls;
 using ClrVpin.Controls.FolderSelection;
