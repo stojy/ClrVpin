4cdd1db89046d636358d3bf342d92ead2edc54a5
stoj <tibor@stoj.net>
2022-09-06 23:47:37 +0800

    performance: fixed binding error with BindingProxy

--- ClrVpin/Controls/BindingProxyFolder.cs
+++ ClrVpin/Controls/BindingProxyFolder.cs
@@ -7,7 +7,12 @@ namespace ClrVpin.Controls;
 // - this class creates a proxy object which stores the data context so it can be referenced elsewhere in xaml
 // - Freezable class accommodates this because it can capture the DataContext despite not being in the visual tree
 // - https://social.technet.microsoft.com/wiki/contents/articles/31422.wpf-passing-a-data-bound-value-to-a-validation-rule.aspx
-public class BindingProxyFolder : DependencyObject
+// - deriving from FrameworkElement instead of freezable to avoid..
+//   1. misleading(?) binding error at runtime
+//      "Cannot find governing FrameworkElement or FrameworkContentElement for target element"
+//      https://stackoverflow.com/questions/40507978/datatemplate-binding-spam-output-window-with-error-cannot-find-governing-framew
+//   2. warnings about resource can be frozen.. which for reasons unknown, i'm unable to get working
+public class BindingProxyFolder : FrameworkElement
 {
     public FolderTypeDetail Data
     {
