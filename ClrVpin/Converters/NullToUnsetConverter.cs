27ebd8d4388a907d77987b91c0f5434259ab8d95
stoj <tibor@stoj.net>
2022-10-29 16:31:52 +0800

    importer: fix image url binding issue by explicitly guarding against a null value

---
+++ ClrVpin/Converters/NullToUnsetConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ClrVpin.Converters;
+
+public class NullToUnsetConverter :IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        // return UnsetValue for those controls that don't handle binding to null values, e.g. Image.Source
+        // - https://stackoverflow.com/a/5628347/227110
+        return value ?? DependencyProperty.UnsetValue;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
