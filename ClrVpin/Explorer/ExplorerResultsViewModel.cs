5bd827dc570be14a4ca64ef5dc77351cf0eea09d
stoj <tibor@stoj.net>
2022-11-21 22:13:33 +0800

    explorer: added plumbing for the window layout

---
+++ ClrVpin/Explorer/ExplorerResultsViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using ClrVpin.Controls;
+using ClrVpin.Models.Settings;
+using ClrVpin.Models.Shared;
+using ClrVpin.Models.Shared.Game;
+using ClrVpin.Shared;
+using PropertyChanged;
+using Utils;
+
+namespace ClrVpin.Explorer
+{
+    [AddINotifyPropertyChangedInterface]
+    public class ExplorerResultsViewModel : ResultsViewModel
+    {
+        public ExplorerResultsViewModel(ObservableCollection<LocalGame> games)
+        {
+            Games = games;
+            Initialise();
+        }
+
+        public async Task Show(Window parentWindow, double left, double top, double width)
+        {
+            Window = new MaterialWindowEx
+            {
+                Owner = parentWindow,
+                Title = "Results",
+                Left = left,
+                Top = top,
+                Width = width,
+                Height = (Model.ScreenWorkArea.Height - WindowMargin - WindowMargin) * 0.8,
+                Content = this,
+                Resources = parentWindow.Resources,
+                ContentTemplate = parentWindow.FindResource("ResultsTemplate") as DataTemplate
+            };
+            Window.Show();
+
+            await ShowSummary();
+        }
+
+        protected override IList<FeatureType> CreateAllContentFeatureTypes()
+        {
+            // show all content types, but assign enabled and active based on the cleaner configuration
+            var featureTypes = Settings.GetFixableContentTypes().Select(contentType => new FeatureType((int)contentType.Enum)
+            {
+                Description = contentType.Description,
+                Tip = contentType.Tip,
+
+                // todo; use id
+                IsSupported = Settings.Cleaner.SelectedCheckContentTypes.Contains(contentType.Description),
+                IsActive = Settings.Cleaner.SelectedCheckContentTypes.Contains(contentType.Description),
+                SelectedCommand = new ActionCommand(UpdateHitsView)
+            }).ToList();
+
+            return featureTypes.Concat(new[] { FeatureType.CreateSelectAll(featureTypes) }).ToList();
+        }
+
+        protected override IList<FeatureType> CreateAllHitFeatureTypes()
+        {
+            // show all hit types, but assign enabled and active based on the cleaner configuration
+            // - for completeness the valid hits are also visible, but disabled by default since no fixes were required
+            var featureTypes = StaticSettings.AllHitTypes.Select(hitType => new FeatureType((int)hitType.Enum)
+            {
+                Description = hitType.Description,
+                IsSupported = Settings.Cleaner.SelectedCheckHitTypes.Contains(hitType.Enum) || hitType.Enum == HitTypeEnum.CorrectName,
+                IsActive = Settings.Cleaner.SelectedCheckHitTypes.Contains(hitType.Enum),
+                SelectedCommand = new ActionCommand(UpdateHitsView)
+            }).ToList();
+
+            return featureTypes.Concat(new[] { FeatureType.CreateSelectAll(featureTypes) }).ToList();
+        }
+
+
+        private async Task ShowSummary()
+        {
+            var validHits = Games.SelectMany(x => x.Content.ContentHitsCollection).SelectMany(x => x.Hits).Where(x => x.Type == HitTypeEnum.CorrectName).ToList();
+            var eligibleFiles = Games.Count * Settings.Cleaner.SelectedCheckContentTypes.Count;
+            var missingFilesCount = eligibleFiles - validHits.Count;
+
+            var detail = CreatePercentageStatistic("Missing Files", missingFilesCount, eligibleFiles);
+            var isSuccess = missingFilesCount == 0;
+
+            await (isSuccess ? Notification.ShowSuccess(DialogHostName, "All Files Are Good") : Notification.ShowWarning(DialogHostName, "Missing or Incorrect Files", null, detail));
+        }
+
+        private const int WindowMargin = 0;
+    }
+}
---
+++ ClrVpin/Explorer/ExplorerViewModel.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using ClrVpin.Controls;
+using ClrVpin.Extensions;
+using ClrVpin.Logging;
+using ClrVpin.Models.Shared.Game;
+using ClrVpin.Shared;
+using PropertyChanged;
+
+namespace ClrVpin.Explorer;
+
+[AddINotifyPropertyChangedInterface]
+public class ExplorerViewModel : IShowViewModel
+{
+    public Models.Settings.Settings Settings { get; } = Model.Settings;
+
+    public Window Show(Window parent)
+    {
+        _window = new MaterialWindowEx
+        {
+            Owner = parent,
+            WindowStartupLocation = WindowStartupLocation.CenterScreen,
+            SizeToContent = SizeToContent.WidthAndHeight,
+            Content = this,
+            Resources = parent.Resources,
+            ContentTemplate = parent.FindResource("ExplorerTemplate") as DataTemplate,
+            ResizeMode = ResizeMode.NoResize,
+            Title = "Explorer"
+        };
+
+        _window.Show();
+
+        Start();
+
+        _window.Closed += (_, _) => Model.SettingsManager.Write();
+
+        return _window;
+    }
+
+    private async void Start()
+    {
+        Logger.Info($"\nExplorer started, settings={JsonSerializer.Serialize(Settings)}");
+
+        _window.Hide();
+        Logger.Clear();
+
+        var progress = new ProgressViewModel();
+        progress.Show(_window);
+
+        List<LocalGame> games;
+        try
+        {
+            progress.Update("Loading Database");
+            games = await TableUtils.ReadGamesFromDatabases(Settings.GetSelectedCheckContentTypes());
+            Logger.Info($"Loading database complete, duration={progress.Duration}", true);
+        }
+        catch (Exception)
+        {
+            progress.Close();
+            _window.TryShow();
+            return;
+        }
+
+        progress.Update("Checking Files");
+        var unmatchedFiles = await ExplorerUtils.CheckAsync(games, UpdateProgress);
+
+        progress.Update("Fixing Files");
+        var fixedFiles = await ExplorerUtils.FixAsync(games, Settings.BackupFolder, UpdateProgress);
+
+        progress.Update("Removing Unmatched Files");
+        await ExplorerUtils.RemoveUnmatchedAsync(unmatchedFiles, UpdateProgress);
+
+        // delete empty backup folders - i.e. if there are no files (empty sub-directories are allowed)
+        FileUtils.DeleteActiveBackupFolderIfEmpty();
+
+        progress.Update("Preparing Results");
+        await Task.Delay(1);
+        _games = new ObservableCollection<LocalGame>(games);
+
+        progress.Close();
+
+        await ShowResults(fixedFiles, unmatchedFiles, progress.Duration);
+
+        void UpdateProgress(string detail, float ratioComplete) => progress.Update(null, ratioComplete, detail);
+    }
+
+    private async Task ShowResults(ICollection<FileDetail> fixedFiles, ICollection<FileDetail> unmatchedFiles, TimeSpan duration)
+    {
+        var screenPosition = _window.GetCurrentScreenPosition();
+
+        var width = Model.ScreenWorkArea.Width - WindowMargin;
+        var results = new ExplorerResultsViewModel(_games);
+        var displayTask = results.Show(_window, screenPosition.X, screenPosition.Y + WindowMargin, width);
+
+        var statistics = new ExplorerStatisticsViewModel(_games, duration, fixedFiles, unmatchedFiles);
+        statistics.Show(_window, screenPosition.X + WindowMargin, results.Window.Top + results.Window.Height + WindowMargin);
+
+        var logging = new LoggingViewModel();
+        logging.Show(_window, statistics.Window.Left + statistics.Window.Width + WindowMargin, results.Window.Top + results.Window.Height + WindowMargin, 
+            Model.ScreenWorkArea.Width - statistics.Window.Width - WindowMargin - WindowMargin);
+
+        statistics.Window.Closed += CloseWindows();
+        results.Window.Closed += CloseWindows();
+        logging.Window.Closed += CloseWindows();
+
+        EventHandler CloseWindows()
+        {
+            return (_, _) =>
+            {
+                statistics.Window.Close();
+                results.Close();
+                logging.Close();
+
+                _window.Close();
+            };
+        }
+
+        await displayTask;
+    }
+
+    private ObservableCollection<LocalGame> _games;
+    private MaterialWindowEx _window;
+    private const int WindowMargin = 0;
+}
