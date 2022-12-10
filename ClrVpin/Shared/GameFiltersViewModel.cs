bd443b094b63ee26428a5187a135b4358a7c72c2
stoj <tibor@stoj.net>
2022-12-10 22:51:30 +0800

    explorer: adding filtering.. dynamic filter options and tables

--- ClrVpin/Shared/GameFiltersViewModel.cs
+++ ClrVpin/Shared/GameFiltersViewModel.cs
@@ -5,18 +5,18 @@ namespace ClrVpin.Shared;
 
 public class GameFiltersViewModel
 {
-    public ListCollectionView<string> TablesFilterView { get; set; }
-    public ListCollectionView<string> ManufacturersFilterView { get; set; }
-    public ListCollectionView<string> YearsBeginFilterView { get; set; }
-    public ListCollectionView<string> YearsEndFilterView { get; set; }
-    public ListCollectionView<string> TypesFilterView { get; set; }
-    public ListCollectionView<string> FormatsFilterView { get; set; }
+    public ListCollectionView<string> TablesFilterView { get; set; } = new();
+    public ListCollectionView<string> ManufacturersFilterView { get; set; } = new();
+    public ListCollectionView<string> YearsBeginFilterView { get; set; } = new();
+    public ListCollectionView<string> YearsEndFilterView { get; set; } = new();
+    public ListCollectionView<string> TypesFilterView { get; set; } = new();
+    public ListCollectionView<string> FormatsFilterView { get; set; } = new();
 
-    public ListCollectionView<FeatureType> TableStyleOptionsView { get; set; }
-    public ListCollectionView<FeatureType> TableMatchOptionsView { get; set;}
-    public ListCollectionView<FeatureType> TableAvailabilityOptionsView { get; set;}
-    public ListCollectionView<FeatureType> TableNewContentOptionsView { get; set;}
-    public ListCollectionView<FeatureType> PresetDateOptionsView { get; set;}
+    public ListCollectionView<FeatureType> TableStyleOptionsView { get; set; } = new();
+    public ListCollectionView<FeatureType> TableMatchOptionsView { get; set;} = new();
+    public ListCollectionView<FeatureType> TableAvailabilityOptionsView { get; set;} = new();
+    public ListCollectionView<FeatureType> TableNewContentOptionsView { get; set;} = new();
+    public ListCollectionView<FeatureType> PresetDateOptionsView { get; set;} = new();
 
     public void Refresh()
     {
