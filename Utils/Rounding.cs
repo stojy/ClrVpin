a2914545435eee10241e4dac738937cb3e1459c4
stoj <tibor@stoj.net>
2022-12-15 22:22:00 +0800

    explorer: updated nuget dependencies, round the ratings to the nearest half, and fix the animation to avoid stuttering

---
+++ Utils/Rounding.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Utils;
+
+public static class Rounding
+{
+    public static double? ToHalf(double? value) 
+    {
+        return value == null ? null : Math.Round(value.Value * 2) / 2;
+    }
+}
