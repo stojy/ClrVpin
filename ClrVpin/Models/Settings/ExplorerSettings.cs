50b18d1e4669bd4a09f368db67fa1a73c322872c
stoj <tibor@stoj.net>
2023-01-26 17:57:47 +0800

    explorer: started adding support for new filters.. missing files and stale files

--- ClrVpin/Models/Settings/ExplorerSettings.cs
+++ ClrVpin/Models/Settings/ExplorerSettings.cs
@@ -1,8 +1,14 @@
-﻿namespace ClrVpin.Models.Settings;
+﻿using ClrVpin.Models.Shared;
+
+namespace ClrVpin.Models.Settings;
 
 public class ExplorerSettings : CommonFilterSettings
 {
     public double? SelectedMinRating { get; set; }
     public double? SelectedMaxRating { get; set; }
+
+    // todo; checkbox with list
+    public ContentTypeEnum SelectedTableMissingOptions { get; set; } = ContentTypeEnum.Tables;
+    public ContentTypeEnum SelectedTableStaleOptions { get; set; } = ContentTypeEnum.TableVideos;
 }
 
