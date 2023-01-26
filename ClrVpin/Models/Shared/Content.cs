1eff37b6959c18162cf3fa927b607d61fea56d43
stoj <tibor@stoj.net>
2023-01-26 16:43:38 +0800

    explorer: added IPDB navigation and fixed coumn sorting

--- ClrVpin/Models/Shared/Content.cs
+++ ClrVpin/Models/Shared/Content.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows.Data;
-using ClrVpin.Controls;
 using ClrVpin.Models.Shared.Game;
 using PropertyChanged;
 using Utils.Extensions;
