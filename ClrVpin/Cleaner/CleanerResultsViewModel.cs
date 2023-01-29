8927d98450e7a26b721ff2c2d73070c3ded5d3f1
stoj <tibor@stoj.net>
2023-01-29 21:59:13 +0800

    general: migrating merger and cleaner results to the generic feature options.. also fixed the disabled state of the various checkbox options

--- ClrVpin/Cleaner/CleanerResultsViewModel.cs
+++ ClrVpin/Cleaner/CleanerResultsViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -10,72 +9,74 @@ using ClrVpin.Models.Shared.Game;
 using ClrVpin.Shared;
 using ClrVpin.Shared.FeatureType;
 using PropertyChanged;
-using Utils;
+using Utils.Extensions;
 
-namespace ClrVpin.Cleaner
+namespace ClrVpin.Cleaner;
+
+[AddINotifyPropertyChangedInterface]
+public class CleanerResultsViewModel : ResultsViewModel
 {
-    [AddINotifyPropertyChangedInterface]
-    public class CleanerResultsViewModel : ResultsViewModel
+    public CleanerResultsViewModel(ObservableCollection<LocalGame> games)
     {
-        public CleanerResultsViewModel(ObservableCollection<LocalGame> games)
-        {
-            Games = games;
-            Initialise();
-        }
-
-        public async Task Show(Window parentWindow, double left, double top, double width)
-        {
-            Window = new MaterialWindowEx
-            {
-                Owner = parentWindow,
-                Title = "Results",
-                Left = left,
-                Top = top,
-                Width = width,
-                Height = (Model.ScreenWorkArea.Height - WindowMargin - WindowMargin) / 3,
-                Content = this,
-                Resources = parentWindow.Resources,
-                ContentTemplate = parentWindow.FindResource("ResultsTemplate") as DataTemplate
-            };
-            Window.Show();
-
-            await ShowSummary();
-        }
-
-        protected override ListCollectionView<FeatureType> CreateAllContentFeatureTypesView()
-        {
-            // show all content types, but assign enabled and active based on the cleaner configuration
-            return FeatureOptions.CreateFeatureOptionsSelectionsView(Settings.GetFixableContentTypes(), Settings.Cleaner.SelectedCheckContentTypes, _ => UpdateHitsView());
-        }
-
-        protected override IList<FeatureType> CreateAllHitFeatureTypes()
-        {
-            // show all hit types, but assign enabled and active based on the cleaner configuration
-            // - for completeness the valid hits are also visible, but disabled by default since no fixes were required
-            var featureTypes = StaticSettings.AllHitTypes.Select(hitType => new FeatureType((int)hitType.Enum)
-            {
-                Description = hitType.Description,
-                IsSupported = Settings.Cleaner.SelectedCheckHitTypes.Contains(hitType.Enum) || hitType.Enum == HitTypeEnum.CorrectName,
-                IsActive = Settings.Cleaner.SelectedCheckHitTypes.Contains(hitType.Enum),
-                SelectedCommand = new ActionCommand(UpdateHitsView)
-            }).ToList();
-
-            return featureTypes.Concat(new[] { FeatureOptions.CreateSelectAll(featureTypes) }).ToList();
-        }
-
-
-        private async Task ShowSummary()
-        {
-            var validHits = Games.SelectMany(x => x.Content.ContentHitsCollection).SelectMany(x => x.Hits).Where(x => x.Type == HitTypeEnum.CorrectName).ToList();
-            var eligibleFiles = Games.Count * Settings.Cleaner.SelectedCheckContentTypes.Count;
-            var missingFilesCount = eligibleFiles - validHits.Count;
-
-            var detail = CreatePercentageStatistic("Missing Files", missingFilesCount, eligibleFiles);
-            var isSuccess = missingFilesCount == 0;
-
-            await (isSuccess ? Notification.ShowSuccess(DialogHostName, "All Files Are Good") : Notification.ShowWarning(DialogHostName, "Missing or Incorrect Files", null, detail));
-        }
-
-        private const int WindowMargin = 0;
+        Games = games;
+        Initialise();
     }
+
+    public async Task Show(Window parentWindow, double left, double top, double width)
+    {
+        Window = new MaterialWindowEx
+        {
+            Owner = parentWindow,
+            Title = "Results",
+            Left = left,
+            Top = top,
+            Width = width,
+            Height = (Model.ScreenWorkArea.Height - WindowMargin - WindowMargin) / 3,
+            Content = this,
+            Resources = parentWindow.Resources,
+            ContentTemplate = parentWindow.FindResource("ResultsTemplate") as DataTemplate
+        };
+        Window.Show();
+
+        await ShowSummary();
+    }
+
+    protected override ListCollectionView<FeatureType> CreateAllContentFeatureTypesView()
+    {
+        // show all content types, but assign enabled and active based on the cleaner configuration
+        var contentFeaturesView = FeatureOptions.CreateFeatureOptionsSelectionsView(Settings.GetFixableContentTypes(), Settings.Cleaner.SelectedCheckContentTypes.Clone(), _ => UpdateHitsView());
+        contentFeaturesView.ForEach(featureType =>
+        {
+            featureType.IsSupported = Settings.Cleaner.SelectedCheckContentTypes.Contains(featureType.Description) || featureType.Id == FeatureOptions.SelectAllId;
+        });
+        return contentFeaturesView;
+    }
+
+    protected override ListCollectionView<FeatureType> CreateAllHitFeatureTypesView()
+    {
+        // show all hit types, but assign enabled and active based on the cleaner configuration
+        // - for completeness the valid hits are also visible, but disabled by default since no fixes were required
+        var hitFeaturesView = FeatureOptions.CreateFeatureOptionsSelectionsView(StaticSettings.AllHitTypes, Settings.Cleaner.SelectedCheckHitTypes.Clone(), _ => UpdateHitsView());
+        hitFeaturesView.ForEach(featureType =>
+        {
+            featureType.IsSupported = Settings.Cleaner.SelectedCheckHitTypes.Contains((HitTypeEnum)featureType.Id) ||
+                                      (HitTypeEnum)featureType.Id == HitTypeEnum.CorrectName ||
+                                      featureType.Id == FeatureOptions.SelectAllId;
+        });
+        return hitFeaturesView;
+    }
+
+    private async Task ShowSummary()
+    {
+        var validHits = Games.SelectMany(x => x.Content.ContentHitsCollection).SelectMany(x => x.Hits).Where(x => x.Type == HitTypeEnum.CorrectName).ToList();
+        var eligibleFiles = Games.Count * Settings.Cleaner.SelectedCheckContentTypes.Count;
+        var missingFilesCount = eligibleFiles - validHits.Count;
+
+        var detail = CreatePercentageStatistic("Missing Files", missingFilesCount, eligibleFiles);
+        var isSuccess = missingFilesCount == 0;
+
+        await (isSuccess ? Notification.ShowSuccess(DialogHostName, "All Files Are Good") : Notification.ShowWarning(DialogHostName, "Missing or Incorrect Files", null, detail));
+    }
+
+    private const int WindowMargin = 0;
 }
--- ClrVpin/Shared/ResultsViewModel.cs
+++ ClrVpin/Shared/ResultsViewModel.cs
@@ -9,110 +9,104 @@ using ClrVpin.Models.Shared.Game;
 using Utils;
 using Utils.Extensions;
 
-namespace ClrVpin.Shared
+namespace ClrVpin.Shared;
+
+public abstract class ResultsViewModel
 {
-    public abstract class ResultsViewModel
+    // all games referenced in the DB.. irrespective of hits
+    public ObservableCollection<LocalGame> Games { get; protected init; }
+
+    public ListCollectionView<FeatureType.FeatureType> AllContentFeatureTypesView { get; private set; }
+    public ListCollectionView<FeatureType.FeatureType> AllHitFeatureTypesView { get; private set; }
+    public ListCollectionView<LocalGame> HitGamesView { get; private set; }
+
+    public ICommand ExpandGamesCommand { get; private set; }
+    public string SearchText { get; set; } = "";
+    public ICommand SearchTextCommand { get; set; }
+    public Window Window { get; protected set; }
+
+    public string BackupFolder { get; private set; }
+    public ICommand NavigateToBackupFolderCommand { get; private set; }
+    protected Models.Settings.Settings Settings { get; private set; }
+
+    public void Close()
     {
-        // all games referenced in the DB.. irrespective of hits
-        public ObservableCollection<LocalGame> Games { get; protected init; }
-
-        public ListCollectionView<FeatureType.FeatureType> AllContentFeatureTypesView { get; private set; }
-        public ListCollectionView<FeatureType.FeatureType> AllHitFeatureTypesView { get; private set; }
-        public ListCollectionView<LocalGame> HitGamesView { get; private set; }
-
-        public ICommand ExpandGamesCommand { get; private set; }
-        public string SearchText { get; set; } = "";
-        public ICommand SearchTextCommand { get; set; }
-        public Window Window { get; protected set; }
-
-        public string BackupFolder { get; private set; }
-        public ICommand NavigateToBackupFolderCommand { get; private set; }
-        protected Models.Settings.Settings Settings { get; private set; }
-
-        public void Close()
-        {
-            Window.Close();
-        }
-
-        protected void Initialise()
-        {
-            Settings = Model.Settings;
-
-            _allContentFeatureTypes = 
-            AllContentFeatureTypesView = CreateAllContentFeatureTypesView();
-
-            _allHitFeatureTypes = CreateAllHitFeatureTypes();
-            AllHitFeatureTypesView = new ListCollectionView<FeatureType.FeatureType>(_allHitFeatureTypes.ToList());
-
-            SearchTextCommand = new ActionCommand(SearchTextChanged);
-            ExpandGamesCommand = new ActionCommand<bool>(ExpandItems);
-
-            BackupFolder = FileUtils.ActiveBackupFolder;
-            NavigateToBackupFolderCommand = new ActionCommand(NavigateToBackupFolder);
-
-            UpdateStatus(Games);
-            InitView();
-        }
-
-        protected abstract ListCollectionView<FeatureType.FeatureType> CreateAllContentFeatureTypesView();
-        protected abstract IList<FeatureType.FeatureType> CreateAllHitFeatureTypes();
-
-        protected void UpdateHitsView()
-        {
-            Games.ForEach(game => game.Content.HitsView.Refresh());
-            HitGamesView.Refresh();
-        }
-
-        protected static string CreatePercentageStatistic(string title, int count, int totalCount)
-        {
-            var percentage = totalCount == 0 ? 0 : 100f * count / totalCount;
-            return $"{title}:  {count} of {totalCount} ({percentage:F2}%)";
-        }
-
-        private void NavigateToBackupFolder() => Process.Start("explorer.exe", BackupFolder);
-
-        private void UpdateStatus(IEnumerable<LocalGame> games)
-        {
-            games.ForEach(game =>
-            {
-                // update status of each game based AND filter the view based on the selected content and/or hit criteria
-                game.Content.Update(
-                    () => _allContentFeatureTypes.Where(x => x.IsActive).Select(x => x.Id),
-                    () => _allHitFeatureTypes.Where(x => x.IsActive).Select(x => x.Id));
-            });
-        }
-
-        private void ExpandItems(bool expand)
-        {
-            _hitGames.ForEach(game => game.ViewState.IsExpanded = expand);
-            HitGamesView.Refresh();
-        }
-
-        private void InitView()
-        {
-            _hitGames = new ObservableCollection<LocalGame>(Games.Where(game => game.Content.Hits.Count > 0));
-            HitGamesView = new ListCollectionView<LocalGame>(_hitGames);
-
-            // text filter
-            HitGamesView.Filter += localGame =>
-            {
-                // only display games that have hits AND those hits haven't already been filtered out (e.g. filtered on content or hit type)
-                if (localGame.Content.HitsView.Count == 0)
-                    return false;
-
-                // return hits based on description match against the search text
-                return string.IsNullOrEmpty(SearchText) || localGame.Game.Description.ToLower().Contains(SearchText.ToLower());
-            };
-        }
-
-        private void SearchTextChanged() => HitGamesView.RefreshDebounce();
-
-        // games referenced in the DB that have hits
-        private ObservableCollection<LocalGame> _hitGames;
-
-        private IEnumerable<FeatureType.FeatureType> _allContentFeatureTypes;
-        private IEnumerable<FeatureType.FeatureType> _allHitFeatureTypes;
-
-        protected const string DialogHostName = "ResultsDialog";
+        Window.Close();
     }
+
+    protected void Initialise()
+    {
+        Settings = Model.Settings;
+
+        AllContentFeatureTypesView = CreateAllContentFeatureTypesView();
+
+        AllHitFeatureTypesView = CreateAllHitFeatureTypesView();
+
+        SearchTextCommand = new ActionCommand(SearchTextChanged);
+        ExpandGamesCommand = new ActionCommand<bool>(ExpandItems);
+
+        BackupFolder = FileUtils.ActiveBackupFolder;
+        NavigateToBackupFolderCommand = new ActionCommand(NavigateToBackupFolder);
+
+        UpdateStatus(Games);
+        InitView();
+    }
+
+    protected abstract ListCollectionView<FeatureType.FeatureType> CreateAllContentFeatureTypesView();
+    protected abstract ListCollectionView<FeatureType.FeatureType> CreateAllHitFeatureTypesView();
+
+    protected void UpdateHitsView()
+    {
+        Games.ForEach(game => game.Content.HitsView.Refresh());
+        HitGamesView.Refresh();
+    }
+
+    protected static string CreatePercentageStatistic(string title, int count, int totalCount)
+    {
+        var percentage = totalCount == 0 ? 0 : 100f * count / totalCount;
+        return $"{title}:  {count} of {totalCount} ({percentage:F2}%)";
+    }
+
+    private void NavigateToBackupFolder() => Process.Start("explorer.exe", BackupFolder);
+
+    private void UpdateStatus(IEnumerable<LocalGame> games)
+    {
+        games.ForEach(game =>
+        {
+            // update status of each game based AND filter the view based on the selected content and/or hit criteria
+            game.Content.Update(
+                () => AllContentFeatureTypesView.Where(x => x.IsActive).Select(x => x.Id),
+                () => AllHitFeatureTypesView.Where(x => x.IsActive).Select(x => x.Id));
+        });
+    }
+
+    private void ExpandItems(bool expand)
+    {
+        _hitGames.ForEach(game => game.ViewState.IsExpanded = expand);
+        HitGamesView.Refresh();
+    }
+
+    private void InitView()
+    {
+        _hitGames = new ObservableCollection<LocalGame>(Games.Where(game => game.Content.Hits.Count > 0));
+        HitGamesView = new ListCollectionView<LocalGame>(_hitGames);
+
+        // text filter
+        HitGamesView.Filter += localGame =>
+        {
+            // only display games that have hits AND those hits haven't already been filtered out (e.g. filtered on content or hit type)
+            if (localGame.Content.HitsView.Count == 0)
+                return false;
+
+            // return hits based on description match against the search text
+            return string.IsNullOrEmpty(SearchText) || localGame.Game.Description.ToLower().Contains(SearchText.ToLower());
+        };
+    }
+
+    private void SearchTextChanged() => HitGamesView.RefreshDebounce();
+
+    // games referenced in the DB that have hits
+    private ObservableCollection<LocalGame> _hitGames;
+
+    protected const string DialogHostName = "ResultsDialog";
 }
