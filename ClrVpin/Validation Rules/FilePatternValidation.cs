0c911e5d46fe455515f1072d60f3de13223fd4e1
stoj <tibor@stoj.net>
2023-03-08 22:27:51 +0800

    settings: fixed foler suffix valiation rule responsible for ensuring the media folder name matches the content type

--- ClrVpin/Validation Rules/FilePatternValidation.cs
+++ ClrVpin/Validation Rules/FilePatternValidation.cs
@@ -23,8 +23,11 @@ public class FilePatternValidation : ValidationRule
 
 // validation property must be added as a dependency property to support binding
 // - separate class used because we can't add dependency property to a ValidationRule, because ValidationRule is not a DependencyObject (e.g. UserControl)!
-// - instead, we associate an instance of this class to the validation rule as a POCO property
+// - instead..
+//   - create a 'property class' which itself is a DependencyObject
+//   - add an instance of this class to the validation rule (as a POCO property) and let the xaml bind to the contained property which is a dependency property
 // - https://social.technet.microsoft.com/wiki/contents/articles/31422.wpf-passing-a-data-bound-value-to-a-validation-rule.aspx
+// - https://stackoverflow.com/questions/3862385/wpf-validationrule-with-dependency-property
 [AddINotifyPropertyChangedInterface]
 public class Args : DependencyObject
 {
