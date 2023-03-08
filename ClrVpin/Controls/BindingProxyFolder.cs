26e24ba4b1e1a9955b0fb6853558d9b1f5a8f318
stoj <tibor@stoj.net>
2023-03-08 22:27:51 +0800

    settings: fixed foler suffix valiation rule responsible for ensuring the media folder name matches the content type

--- ClrVpin/Controls/BindingProxyFolder.cs
+++ ClrVpin/Controls/BindingProxyFolder.cs
@@ -8,14 +8,18 @@ namespace ClrVpin.Controls;
 // - this class creates a proxy object which stores the data context so it can be referenced elsewhere in xaml by the element that has no DataContext of it's own.. poor little guy!
 // - Freezable class accommodates this because it can capture the DataContext despite not being in the visual tree
 // - https://social.technet.microsoft.com/wiki/contents/articles/31422.wpf-passing-a-data-bound-value-to-a-validation-rule.aspx
-// - deriving from FrameworkElement instead of freezable to avoid..
+
+// - REMOVED 8/3/23 SINCE THIS DOES NOT WORK!!
+//   deriving from FrameworkElement instead of freezable to avoid..
 //   1. misleading(?) binding error at runtime
 //      "Cannot find governing FrameworkElement or FrameworkContentElement for target element"
 //      https://stackoverflow.com/questions/40507978/datatemplate-binding-spam-output-window-with-error-cannot-find-governing-framew
 //   2. warnings about resource can be frozen.. which for reasons unknown, i'm unable to get working
 [AddINotifyPropertyChangedInterface]
-public class BindingProxyFolder : FrameworkElement
+public class BindingProxyFolder : Freezable
 {
+    protected override Freezable CreateInstanceCore() => new BindingProxyFolder();
+
     public FolderTypeDetail Data
     {
         get => (FolderTypeDetail)GetValue(DataProperty);
