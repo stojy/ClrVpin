87780e279b02ce32c88e8d7e7823b5d9df26fafd
stoj <tibor@stoj.net>
2023-03-11 13:58:06 +0800

    settings: don't disable top level buttons if the optional folders aren't correctly configured

--- ClrVpin/Models/Settings/SettingsManager.cs
+++ ClrVpin/Models/Settings/SettingsManager.cs
@@ -6,74 +6,72 @@ using PropertyChanged;
 using Utils.Extensions;
 using Path = System.IO.Path;
 
-namespace ClrVpin.Models.Settings
+namespace ClrVpin.Models.Settings;
+
+[AddINotifyPropertyChangedInterface]
+public class SettingsManager
 {
-    [AddINotifyPropertyChangedInterface]
-    public class SettingsManager
+    private SettingsManager()
     {
-        private SettingsManager()
-        {
-            SettingsHelper.CreateRootFolder(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Stoj", "ClrVpin"));
+        SettingsHelper.CreateRootFolder(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Stoj", "ClrVpin"));
 
-            (_defaultSettings, _) = SettingsHelper.Read<DefaultSettings>(null);
-            (Settings, ResetReason) = SettingsHelper.Read<Settings>(_defaultSettings);
+        (_defaultSettings, _) = SettingsHelper.Read<DefaultSettings>(null);
+        (Settings, ResetReason) = SettingsHelper.Read<Settings>(_defaultSettings);
             
-            // update config after read to store any new properties that have been added
-            Write();
+        // update config after read to store any new properties that have been added
+        Write();
 
-            UpdateIsValid();
-        }
-
-        public string ResetReason { get; private set; }
-        public bool IsValid { get; private set; }
-        public Settings Settings { get; private set; }
-
-        public static SettingsManager Create()
-        {
-            return _sessionManager ??= new SettingsManager();
-        }
-
-        public void Reset()
-        {
-            // reset Settings, but keep defaultSettings unchanged.. i.e. the defaultSettings are used to seed the reset Settings
-            Settings = SettingsHelper.Reset<Settings>(_defaultSettings);
-            ResetReason = "Requested by user.";
-        }
-
-        public void Write()
-        {
-            // write default and settings
-            SettingsHelper.Write(_defaultSettings);
-            SettingsHelper.Write(Settings);
-
-            UpdateIsValid();
-        }
-
-        private void UpdateIsValid()
-        {
-            // following special folders are considered mandatory
-            var specialFolders = new List<string>
-            {
-                //Settings.PinballFolder,
-                //Settings.FrontendFolder,
-                Settings.BackupFolder
-            };
-
-            // assume all folders are invalid
-            Settings.GetAllContentTypes().ForEach(x => x.IsFolderValid = false);
-
-            // verify content folder types that are either..
-            // - required
-            // - optional, but have been defined
-            var contentTypesToVerify = Settings.GetAllContentTypes().Where(x => x.IsFolderRequired  || !string.IsNullOrEmpty(x.Folder)).ToList();
-            contentTypesToVerify.ForEach(x => x.IsFolderValid = DoesFolderOrFileExist(x.Folder));
-
-            IsValid = contentTypesToVerify.All(x => x.IsFolderValid) && specialFolders.All(DoesFolderOrFileExist);
-        }
-
-        private static bool DoesFolderOrFileExist(string path) => Directory.Exists(path) || File.Exists(path);
-
-        private readonly DefaultSettings _defaultSettings;
-        private static SettingsManager _sessionManager;
+        UpdateIsValid();
     }
+
+    public string ResetReason { get; private set; }
+    public bool IsValid { get; private set; }
+    public Settings Settings { get; private set; }
+
+    public static SettingsManager Create()
+    {
+        return _sessionManager ??= new SettingsManager();
+    }
+
+    public void Reset()
+    {
+        // reset Settings, but keep defaultSettings unchanged.. i.e. the defaultSettings are used to seed the reset Settings
+        Settings = SettingsHelper.Reset<Settings>(_defaultSettings);
+        ResetReason = "Requested by user.";
+    }
+
+    public void Write()
+    {
+        // write default and settings
+        SettingsHelper.Write(_defaultSettings);
+        SettingsHelper.Write(Settings);
+
+        UpdateIsValid();
+    }
+
+    private void UpdateIsValid()
+    {
+        // following special folders are considered mandatory
+        var specialFolders = new List<string>
+        {
+            //Settings.PinballFolder,
+            //Settings.FrontendFolder,
+            Settings.BackupFolder
+        };
+
+        // assume all folders are invalid
+        Settings.GetAllContentTypes().ForEach(x => x.IsFolderValid = false);
+
+        // requirements for the overall settings to be valid..
+        // - required folders are specified and exist.. optional folders are ignored
+        var contentTypesToVerify = Settings.GetAllContentTypes().Where(x => x.IsFolderRequired).ToList();
+        contentTypesToVerify.ForEach(x => x.IsFolderValid = DoesFolderOrFileExist(x.Folder));
+
+        IsValid = contentTypesToVerify.All(x => x.IsFolderValid) && specialFolders.All(DoesFolderOrFileExist);
+    }
+
+    private static bool DoesFolderOrFileExist(string path) => Directory.Exists(path) || File.Exists(path);
+
+    private readonly DefaultSettings _defaultSettings;
+    private static SettingsManager _sessionManager;
 }
