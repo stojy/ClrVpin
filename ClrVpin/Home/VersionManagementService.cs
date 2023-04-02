3c78b1273c1aacfd22b9ca18eb3b19e1af989d9f
stoj <tibor@stoj.net>
2023-04-02 21:22:52 +0800

    general: handle exceptions when checking for version updates

--- ClrVpin/Home/VersionManagementService.cs
+++ ClrVpin/Home/VersionManagementService.cs
@@ -23,16 +23,23 @@ public static class VersionManagementService
 
     public static async Task CheckAndHandle(MaterialWindowEx parent = null, bool showIfNoUpdateExists = false)
     {
-        var releases = await VersionManagement.Check(Model.SettingsManager.Settings.Guid, "stojy", "ClrVpin", msg => Logger.Info($"Version checking: {msg}"));
+        try
+        {
+            var releases = await VersionManagement.Check(Model.SettingsManager.Settings.Guid, "stojy", "ClrVpin", msg => Logger.Info($"Version checking: {msg}"));
 
-        if (releases.Any())
-            await VersionManagementView.Show(releases, parent);
-        else if (showIfNoUpdateExists) 
-            await Notification.ShowSuccess("HomeDialog", "No Updates Available");
+            if (releases.Any())
+                await VersionManagementView.Show(releases, parent);
+            else if (showIfNoUpdateExists)
+                await Notification.ShowSuccess("HomeDialog", "No Updates Available");
 
-        // update last check AFTER processing to ensure the msi installer (if invoked) doesn't update the version (since it causes the process to exit before it reaches here)
-        // - intention is that the new version when it starts up will perform another version check to ensure everything is up to date (which it should be!)
-        Model.SettingsManager.Settings.LastCheckForNewVersion = DateTime.Now;
-        Model.SettingsManager.Write();
+            // update last check AFTER processing to ensure the msi installer (if invoked) doesn't update the version (since it causes the process to exit before it reaches here)
+            // - intention is that the new version when it starts up will perform another version check to ensure everything is up to date (which it should be!)
+            Model.SettingsManager.Settings.LastCheckForNewVersion = DateTime.Now;
+            Model.SettingsManager.Write();
+        }
+        catch (Exception e)
+        {
+            await Notification.ShowError("HomeDialog", "Update Check Failed", "", e.Message, true, true);
+        }
     }
 }
