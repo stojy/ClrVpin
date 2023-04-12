2857838ca1112f0656642b4d1dceeb006c6e043f
stoj <tibor@stoj.net>
2023-04-12 23:07:15 +0800

    general: extract vb script from vpx file

---
+++ ClrVpin/Shared/Utils/TableUtils.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using OpenMcdf;
+
+namespace ClrVpin.Shared.Utils;
+
+public static class TableUtils
+{
+    public static string GetRomOrPup(string tableFile)
+    {
+        // the vpx files are encoded as CFBF (compound file binary format), which is a COM structured storage for housing files/folders within a single file
+        // - openmcdf supports these files.. presumably because CFBF is the same as as MCDF (Microsoft Compound Document Files) ??
+        // - refer https://en.wikipedia.org/wiki/Compound_File_Binary_Format
+        //         https://github.com/ironfede/openmcdf
+
+        var script = GetScript(tableFile);
+
+        var rom = ParseScript(script);
+
+        return rom;
+    }
+
+    private static string GetScript(string tableFile)
+    {
+        string script = null;
+        
+        using var cf = new CompoundFile(tableFile);
+        if (cf.RootStorage.TryGetStorage("GameStg", out var gameStorage))
+        {
+            var stream = gameStorage.GetStream("GameData");
+            var data = stream.GetData();
+            script = Encoding.UTF8.GetString(data);
+            //var scriptLines = script.Split(new[] { "\r\n", "\n\r", "\r", "\n" }, StringSplitOptions.None);
+        }
+
+        return script;
+    }
+
+    private static string ParseScript(string script)
+    {
+        return null;
+    }
+}
