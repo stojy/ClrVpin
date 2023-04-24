d41996f1ea4c2e1dbbac3e246346577b2df7aad3
stoj <tibor@stoj.net>
2023-04-24 22:07:41 +0800

    cli: integrate System.CommandLine

--- ClrVpin/Program.cs
+++ ClrVpin/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.CommandLine;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 
@@ -10,43 +12,49 @@ public static class Program
     public static int Main(string[] args)
     {
         if (args.Any())
-            return RunConsole();
+            return RunConsole(args);
 
         App.Main();
         return 0;
     }
 
-    private static int RunConsole()
+    private static int RunConsole(string[] args)
     {
-        // run a console as either..
-        // a. new console - if started from UI, e.g. explorer
-        // b. existing console - if started from a console
-        // - refer https://stackoverflow.com/questions/32204195/wpf-and-commandline-app-in-the-same-executable
-        // - not specified in the posts, but the console must be created/used BEFORE App.Main is started
-        // - not done within App.OnStartup because whilst creates/attaches console works, the WriteLine does not
         try
         {
-            const int attachParentProcess = -1;
-            if (AttachConsole(attachParentProcess))
-            {
-                Console.WriteLine("attached");
-                Console.WriteLine("baby");
-                Console.ReadKey();
-            }
-            else
-            {
+            // open console via either..
+            // a. new console - if started from UI, e.g. explorer
+            // b. existing console - if started from a console
+            // - refer https://stackoverflow.com/questions/32204195/wpf-and-commandline-app-in-the-same-executable
+            // - not specified in the posts, but the console must be created/used BEFORE App.Main is started
+            // - not done within App.OnStartup because whilst creates/attaches console works, the WriteLine does not
+            if (!AttachConsole(-1)) // -1 = attach parent process
                 AllocConsole();
-                Console.WriteLine("allocated");
-                Console.WriteLine("baby");
-                Console.ReadKey();
-            }
+
+            // setup CLI
+            return ProcessCommandLine(args);
         }
         finally
         {
             FreeConsole();
         }
+    }
 
-        return 0;
+    private static int ProcessCommandLine(string[] args)
+    {
+        var tableOption = new Option<FileInfo>("--table", "The table file to inspect") { IsRequired = true };
+        tableOption.AddAlias("--t");
+
+        var inspectCommand = new Command("inspect", "Inspect file details");
+        inspectCommand.AddAlias("i");
+        inspectCommand.AddOption(tableOption);
+
+        var rootCommand = new RootCommand("ClrVpin Command Line Interface");
+        rootCommand.AddCommand(inspectCommand);
+
+        inspectCommand.SetHandler(table => { Console.WriteLine($"my table: {table}"); }, tableOption);
+
+        return rootCommand.Invoke(args);
     }
 
     [DllImport("kernel32")]
